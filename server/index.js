eff25bfc3299a364882cbbe15f77bc25c44139d0
danishgrappus <127500304+danishgrappus@users.noreply.github.com>
2024-04-14 00:08:08 +0530

    Add socket io

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import { createServer } from 'node:http';
-import cors from "cors"
+import cors from "cors";
+import { Server } from 'socket.io';
+
 const app = express();
 app.use(cors())
 const server = createServer(app);
+const io = new Server(server,{
+    cors:{
+        origin:["http://127.0.0.1:5173","https://rps-game-liart-eight.vercel.app/"]
+    }
+})
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
@@ -12,6 +19,15 @@ app.get('/test', (req, res) => {
   res.send({res:"hii"});
 });
 
+io.on('connection', (socket) => {
+    socket.on('connect',()=>{
+        console.log('a new user arrived')
+    })
+    socket.on('disconnect',()=>{
+        console.log("user has disconnected")
+    })
+  });
+
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+  console.log('server running...');
 });
