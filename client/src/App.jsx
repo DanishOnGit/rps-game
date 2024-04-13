c4606477f1cf03c93089d5f50f4035ff4528af01
danishgrappus <127500304+danishgrappus@users.noreply.github.com>
2024-04-13 22:20:36 +0530

    Refactor

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   const getdata=()=>{
-    const res = fetch("http://localhost:3000/test").then(res=>res.json()).then(data=>console.log(data)).catch(console.error)
+    const res = fetch(`${process.env.REACT_APP_API_URL}/test`).then(res=>res.json()).then(data=>console.log(data)).catch(console.error)
   }
 
   useEffect(()=>{
